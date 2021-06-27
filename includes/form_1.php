50ed41d0e98df247456033f2cc8e7eb066d6725f
Aurelie Gautier Bartolo <aurelie.gautierbartolo@gmail.com>
2021-06-27 16:08:24 +0200

    Fix de tous les liens

--- includes/form_1.php
+++ includes/form_1.php
@@ -9,14 +9,14 @@
 	$input_504 = $_POST['input_504'];
 	$message = $_POST['message'];
 	
-	$to = 'aurelie.gautierbartolo@gmail.com'; // Email submissions are sent to this email
+	$to = 'contact@lachouetteagence.com'; // Email submissions are sent to this email
 
 	// Create email	
-	$email_subject = "Message from your website";
-	$email_body = "You have received a new message. \n\n".
+	$email_subject = "Un message depuis votre site Internet";
+	$email_body = "Vous avez reçu un message. \n\n".
 				  "Name: $name \nEmail: $email \nInput_504: $input_504 \nMessage: $message \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
-	$headers .= "From: aurelie.gautierbartolo@gmail.com\n";
+	$headers .= "From: contact@lachouetteagence.com\n";
 	$headers .= "Reply-To: $input_504";	
 	
 	mail($to,$email_subject,$email_body,$headers); // Post message
